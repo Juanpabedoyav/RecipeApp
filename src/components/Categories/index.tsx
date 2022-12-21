406ae33ccb0ef733d59c0a0564227d0163ca46c3
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2022-12-21 00:43:12 -0500

    add: Categories components

---
+++ src/components/Categories/index.tsx
@@ -0,0 +1,20 @@
+import styles from "styles/Categories.module.scss";
+
+export const Categories = () => {
+  return (
+    <div className={styles.Categories}>
+        
+        <ul className={styles['list-categories']}>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+
+        </ul>
+
+
+    </div>
+  )
+}
