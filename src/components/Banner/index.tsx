be8613115d36bad8ba55c490de90c0beab162fa2
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2022-12-20 22:53:28 -0500

    add: loading lazy to image banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,7 +4,7 @@ import banner from "assets/bcq.jpg";
 export const Banner = () => {
   return (
     <div className={styles.Banner}>
-      <img src={banner} alt="" />
+      <img src={banner} alt="banner recipeApp" loading="lazy" />
       <div className={styles.Presentation}>
         <p>Recetas</p>
         <p>Para todos</p>
