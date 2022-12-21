3a1e8b138d245b7cee84ad91062e90cb7f1e1f54
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2022-12-20 22:06:00 -0500

    add: logos to the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,22 @@
-import { Link } from 'react-router-dom'
-import styles from 'styles/Header.module.scss'
+// import { Link } from 'react-router-dom'
+import styles from "styles/Header.module.scss";
+import homeLogo from "assets/icons/ic_home.svg";
+import logoCompany from "assets/logo.png";
 
 export const Header = () => {
   return (
     <header className={styles.Header}>
-        <nav>
-            <ul>
-                <li>
-                  <img src="" alt="" />
-                </li>
-                <li>
-                  <Link to={'/'}/>
-                </li>
-            </ul>
-        </nav>
-        
-        </header>
-  )
-}
+      <nav className={styles.Nav}>
+        <a href="">
+          <figure>
+            <img src={logoCompany} alt="back to home" />
+          </figure>
+        </a>
+
+        <a href="">
+          <img src={homeLogo} alt="back to home" />
+        </a>
+      </nav>
+    </header>
+  );
+};
