41571db7f78794c65f86809dce602c0ea584616b
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2022-12-21 19:35:40 -0500

    add: icons into the cards

--- src/components/Recipes/index.tsx
+++ src/components/Recipes/index.tsx
@@ -1,4 +1,10 @@
 import styles from "styles/Card.module.scss";
+import ojingeo5 from 'assets/Ojingeo-muchim-5.png'
+import ojingeo1 from 'assets/Ojingeo-muchim-1.png'
+import ojingeo from 'assets/Ojingeo-muchim.png'
+import cherry from 'assets/cherry.png'
+import start from 'assets/icons/ic_star.svg'
+import like from 'assets/icons/ic-favorite.svg'
 
 export const Recipes = () => {
   return (
@@ -6,35 +12,58 @@ export const Recipes = () => {
       <h1 className={styles['recipes-title']}>Nuevas Recetas</h1>
       <section className={styles['card-list']}>
         <div className={styles.Card}>
-
+          <div className={styles['card-detail']}>
+            <img src={ojingeo5} alt="" />
+            <p>Ojingeo <small>Muchim</small></p>
+          </div>
+          <div className={styles.Icons}>
+            <div className={styles['icons-start']}>
+              <img src={start} alt="favorite start" />
+              <p>5.0</p>
+            </div>
+            <img src={like} alt="like" />
+          </div>
         </div>
         <div className={styles.Card}>
-
+          <div className={styles['card-detail']}>
+            <img src={ojingeo1} alt="" />
+            <p>Ojingeo <small>Muchim</small></p>
+          </div>
+          <div className={styles.Icons}>
+            <div className={styles['icons-start']}>
+              <img src={start} alt="favorite start" />
+              <p>5.0</p>
+            </div>
+            <img src={like} alt="like" />
+          </div>
         </div>
         <div className={styles.Card}>
-
+          <div className={styles['card-detail']}>
+            <img src={ojingeo} alt="" />
+            <p>Ojingeo <small>Muchim</small></p>
+          </div>
+          <div className={styles.Icons}>
+            <div className={styles['icons-start']}>
+              <img src={start} alt="favorite start" />
+              <p>5.0</p>
+            </div>
+            <img src={like} alt="like" />
+          </div>
         </div>
         <div className={styles.Card}>
-
+          <div className={styles['card-detail']}>
+            <img src={cherry} alt="" />
+            <p>Ojingeo <small>Muchim</small></p>
+          </div>
+          <div className={styles.Icons}>
+            <div className={styles['icons-start']}>
+              <img src={start} alt="favorite start" />
+              <p>5.0</p>
+            </div>
+            <img src={like} alt="like" />
+          </div>
         </div>
-        <div className={styles.Card}>
 
-        </div>
-        <div className={styles.Card}>
-
-</div>
-<div className={styles.Card}>
-
-</div>
-<div className={styles.Card}>
-
-</div>
-<div className={styles.Card}>
-
-</div>
-<div className={styles.Card}>
-
-</div>
       </section>
 
     </>
